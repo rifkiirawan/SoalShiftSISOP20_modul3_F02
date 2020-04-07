c366120345c8f6dfbea293245e031039ad8618d3
rifkiirawan <57948738+rifkiirawan@users.noreply.github.com>
2020-04-07 13:01:42 +0700

    Update soal4b.c

--- soal4/soal4b.c
+++ soal4/soal4b.c
@@ -2,52 +2,53 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <string.h>
 #include <pthread.h>
 
-void jumlah (int angka){
-  int i;
-  int res=0;
-  for(i=1;i<=angka;i++){
-    res+=i;
-  }
-  printf("%d",res);
+typedef struct data {
+	int angka;
+	int hasil;
+}data;
+
+int jumlah(int n) 
+{ 
+	int sum=0;
+	for(n;n>0;n--)
+	{
+		sum+=n;
+	}
+	return sum;
 }
 
-void *jum(int angka)
+void *jumn(void* arg)
 {
-  jumlah(angka);
+	data* d = (data*) arg;
+        d->hasil=jumlah(d->angka);
 }
 void main()
 {
-  key_t key = 1234;
-  int *value;
-  int arr[20];
-  int angka, i, j;
-  int shmid = shmget(key, sizeof(int), IPC_CREAT | 0644);
-  value = shmat(shmid, NULL, 0);
+	key_t key = 1234;
+	int *value;
+	int shmid = shmget(key, sizeof(int) * 20, IPC_CREAT | 0644);
+	value = shmat(shmid, NULL, 0);
 
-  pthread_t tid[20];
-  int idx;
-  for (int i = 0;i < 20;i++)
+	pthread_t tid[20];
+	data d[20];
+	for (int i = 0;i < 20;i++)
 	{
-    arr[i]=*value;
-    angka=arr[i];
-    pthread_create(&tid[idx],NULL, &jum, (void*)angka);
-    idx++;
-    sleep(2);
-    if(i%5==4 && i>0)
-    {
-      printf("\n");
-    }
-    else
-    {
-      printf("\t\t");
-    }
-  }
-  for(int i=0; i< idx; i++)
-  {
-    pthread_join(tid[i],NULL);
-  }
-  shmdt(value);
-  shmctl(shmid, IPC_RMID, NULL);
+		d[i].angka = value[i];
+		if (i % 5 == 0) printf("\n");
+		printf("%4d", value[i]);
+		pthread_create(&tid[i],NULL, &jumn, (void*)&d[i]);
+	}
+	printf("\n");
+	for(int i=0; i< 20; i++)
+	{
+		if (i % 5 == 0) printf("\n");
+		printf("%d\t", d[i].hasil);
+		pthread_join(tid[i],NULL);
+	}
+	printf("\n");
+	shmdt(value);
+	shmctl(shmid, IPC_RMID, NULL);
 }
